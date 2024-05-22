884040da2d597915271d4b77b52c2015439fcf99
devinnsegura <devinn.segura@calbaptist.edu>
2024-05-22 16:32:55 -0700

    finished appBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage ({super.key});
@@ -6,7 +7,57 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+          'Breakfast',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.bold
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: GestureDetector(
+        child: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: const Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
+          ),
+        child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg',
+          height: 20,
+          width: 20,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 37,
+          decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)
+            ),
+          child: SvgPicture.asset('assets/icons/dots.svg',
+            height: 5,
+            width: 5,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
